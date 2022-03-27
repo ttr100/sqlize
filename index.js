71ea77e87c0e2667bc18afb634dc12ac3de70288
Hendra <hendra2392@gmail.com>
2022-03-27 15:06:36 +0700

    Add read

--- index.js
+++ index.js
@@ -5,19 +5,40 @@ const myDB = new Sql({
 })
 
 
-// const Product = myDB.define(
-//   'Product', 
-//   {
-//     id: {
-//       type: Sql.DataTypes.INTEGER,
-//       field: 'id'
-//     },
-//     productName: {
-//       type: Sql.DataTypes.STRING,
-//       field: 'product_name'
-//     },
-//     sku: {
-//       type: Sql.DataTypes.STRING,
-//       field: 'sku'
-//     },
-//   }
+const Product = myDB.define(
+  'Product', 
+  {
+    id: {
+      type: Sql.DataTypes.INTEGER,
+      field: 'id',
+      primaryKey: true
+    },
+    productName: {
+      type: Sql.DataTypes.STRING,
+      field: 'product_name'
+    },
+    sku: {
+      type: Sql.DataTypes.STRING,
+      field: 'sku'
+    },
+  },
+  {
+    tableName: 'products',
+    timestamps: false
+  }
+)
+
+
+async function entryPoint(){
+  const allProducts = await Product.findAll();
+
+  for(let i = 0; i < allProducts.length; i++){
+    console.log('-------------------------')
+    console.log(`ID:  ${allProducts[i].id}`)
+    console.log(`Name:  ${allProducts[i].productName}`)
+    console.log(`SKU:  ${allProducts[i].sku}`)
+    console.log('-------------------------')
+  }
+}
+
+entryPoint();
